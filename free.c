030c7206c5d3625d51218fa7b987218c64699d81
beboo22 <mt557829@gmail.com>
2023-05-20 18:17:02 +0300

    free

---
+++ free.c
@@ -0,0 +1,15 @@
+nclude "shell.h"
+/**
+ *  * free_arr - free array
+ *   * @array: array
+ *    */
+void free_arr(char **array)
+{
+	 size_t i;
+
+	  for (i = 0; array[i] != NULL; i++)
+		   {
+			     free(array[i]);
+			      }
+	   free(array);
+}
