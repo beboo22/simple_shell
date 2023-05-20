a6b566417419200db42c85b8b9b3de098dc6b607
beboo22 <mt557829@gmail.com>
2023-05-20 18:55:53 +0300

    argcount

---
+++ arg_counter.c
@@ -0,0 +1,24 @@
+nclude "shell.h"
+/**
+ *  * arg_counter - count the number of arguments
+ *   * @user_input: string of user input
+ *    * Return: number of arguments
+ *     */
+int arg_counter(char *inp)
+{
+	 int i, args, k;
+
+	  args = 1;
+	   i = 0;
+	    k = 0;
+	     while (inp[i] != '\0' && inp[i] != '\n')
+		      {
+			        if (inp[i] != ' ')
+					   k = 1;
+				  if (inp[i] == ' ' && inp[i + 1] != ' '
+						        && inp[i + 1] != '\n' && k == 1)
+					     args++;
+				    i++;
+				     }
+	      return (args);
+}
