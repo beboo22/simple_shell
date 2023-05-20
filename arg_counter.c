c1768e4ef1c6b616e23b1ea39ad458ea0d2dfa0a
beboo22 <mt557829@gmail.com>
2023-05-20 19:35:56 +0300

    argcount

--- arg_counter.c
+++ arg_counter.c
@@ -6,19 +6,19 @@ nclude "shell.h"
  *     */
 int arg_counter(char *inp)
 {
-	 int i, args, k;
+	int i, args, k;
 
-	  args = 1;
-	   i = 0;
-	    k = 0;
-	     while (inp[i] != '\0' && inp[i] != '\n')
-		      {
-			        if (inp[i] != ' ')
-					   k = 1;
-				  if (inp[i] == ' ' && inp[i + 1] != ' '
-						        && inp[i + 1] != '\n' && k == 1)
-					     args++;
-				    i++;
-				     }
-	      return (args);
+	args = 1;
+	i = 0;
+	k = 0;
+	while (inp[i] != '\0' && inp[i] != '\n')
+	{
+		if (inp[i] != ' ')
+			k = 1;
+		if (inp[i] == ' ' && inp[i + 1] != ' '
+				&& inp[i + 1] != '\n' && k == 1)
+			args++;
+		i++;
+	}
+	return (args);
 }
