84346c5c075d31deaacda4ba070aafbf8782d7d0
beboo22 <mt557829@gmail.com>
2023-05-20 19:39:25 +0300

    argcount

--- arg_counter.c
+++ arg_counter.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 /**
  * arg_counter - count the number of arguments
- *   * @user_input: string of user input
+ * @user_input: string of user input
  *    * Return: number of arguments
  *     */
 int arg_counter(char *inp)
